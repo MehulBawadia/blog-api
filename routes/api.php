2e37806f8dbfac9a4044de24a6a76190376170cf
Mehul Bawadia <mehulbawadia@gmail.com>
2022-06-02 11:15:07 +0530

    Code refactored in api routes

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\PostController;
-use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AllPostController;
 use App\Http\Controllers\Api\AllUserController;
-use App\Http\Controllers\Api\UserPostController;
-use App\Http\Controllers\Api\ManagerPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,18 +21,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum', 'role:admin-user'])->prefix('admin')->group(function () {
-    Route::apiResource('posts', AllPostController::class);
-    Route::apiResource('users', AllUserController::class);
-});
-
-Route::middleware(['auth:sanctum', 'role:manager-user'])->prefix('manager')->group(function () {
-    Route::apiResource('posts', AllPostController::class);
-});
-
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware(['auth:sanctum', 'role:regular-user'])->prefix('user')->group(function () {
-    Route::apiResource('posts', AllPostController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::middleware('role:admin-user')->prefix('admin')->group(function () {
+        Route::apiResource('users', AllUserController::class);
+        Route::apiResource('posts', AllPostController::class);
+    });
+
+    Route::middleware('role:manager-user')->prefix('manager')->group(function () {
+        Route::apiResource('posts', AllPostController::class);
+    });
+
+    Route::middleware('role:regular-user')->prefix('user')->group(function () {
+        Route::apiResource('posts', AllPostController::class);
+    });
 });
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
